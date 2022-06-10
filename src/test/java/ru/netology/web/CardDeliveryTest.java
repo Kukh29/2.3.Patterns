f67613d92687640def479425572647c753d5d278
kukh29 <kukh29@gmail.com>
2022-06-10 10:00:07 +0300

    v.7

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,6 +1,10 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
@@ -15,6 +19,16 @@ import static ru.netology.data.DataGenerator.getUserInfo;
 public class CardDeliveryTest {
 
     DataGenerator.UserInfo user = getUserInfo();
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @Test
     public void shouldTest() {
